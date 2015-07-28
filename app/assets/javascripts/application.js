122f25aa140d31e814b95b01992e0b3781da9493
davidmoradi <david@wishpond.com>
2015-07-27 19:46:46 -0700

    Added jQuery Masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.turbolinks
 //= require turbolinks
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require_tree .
