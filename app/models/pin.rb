194bc7652d4c54e0f97efba90a4b04052dad734f
davidmoradi <david@wishpond.com>
2015-07-28 20:12:12 -0700

    Added validations to pin and user model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
+	
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates :image, presence: true
+  validates :description, presence: true
 end
