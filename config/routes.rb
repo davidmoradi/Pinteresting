fe30ac6d5d0266dbf0112e3c4961733c05ba3c13
davidmoradi <david@wishpond.com>
2015-08-08 20:12:46 -0700

    Fixed errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :pins
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "pins#index"
   get "about" => "pages#about"
+
+  devise_scope :user do
+  delete 'sign_out', :to => 'devise/sessions#destroy'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
